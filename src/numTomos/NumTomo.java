bb34e36b13741a8ffee358a41dde66e4d4555abf
Luciano Nahuel Vitale <lunavi97@gmail.com>
2018-07-22 05:12:22 -0300

    Reducción de líneas de código con atributos de la clase

--- src/numTomos/NumTomo.java
+++ src/numTomos/NumTomo.java
@@ -2,8 +2,11 @@ package numTomos;
 
 class NumTomo {
 
+  int i, a;
+  
   String numeracionDeTomos(int n, String d[]) {
-    int i = ( n - 1 ) / 99, a = n - 99 * i;
+    i = ( n - 1 ) / 99;
+    a = n - 99 * i;
     return d [ i ] + ( a > 9 ? "-" : "-0" ) + a;
   }
 
